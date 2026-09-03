1480b249a80e0e4123819244f43e9e3876c4b51f
z <zeenabokegbemi@gmail.com>
2026-09-03 21:25:20 +0100

    normalise stmp

--- Media_platform/settings.py
+++ Media_platform/settings.py
@@ -300,7 +300,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in {
     '1', 'true', 'yes', 'on'}
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '').replace(' ', '')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'noreply@mediaplatform.com'
 EMAIL_TIMEOUT = int(os.environ.get('EMAIL_TIMEOUT', 15))
 
